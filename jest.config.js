e85b08f7d4b0762c6c02d86d1ea51103300260e0
William Pritchard <williamp@kainos.com>
2018-01-15 10:57:11 +0000

    updated the test path ignore to match refactor

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
 	globalSetup: './setup/setup.js',
 	globalTeardown: './setup/teardown.js',
 	testEnvironment: './setup/env.js',
-	testPathIgnorePatterns: ['./page_object/*', './extension/*'],
+	testPathIgnorePatterns: ['./page/*', './extension/*'],
 }
